8d21812856a060bb538f8cf7306c7177bd2b9ea7
aishwaryr <araj713@gmail.com>
2025-09-18 15:54:01 +0530

    project init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	const addr = ":8080"
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
