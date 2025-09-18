c028f8c28a931ab09604743babe8296b94ed3ba3
Aishwary Raj <araj713@gmail.com>
2025-09-18 17:40:13 +0530

    add logger

--- main.go
+++ main.go
@@ -3,14 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"fem.com/movie-site/logger"
 )
 
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+	if err != nil {
+		log.Fatalf("Failed to initialise logger $v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
+}
+
 func main() {
+	logInstance := initializeLogger()
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	const addr = ":8080"
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
 	}
 }
