f950a8e5ae02b8de2c62aa7506649128275ea67f
aishwary raj <araj713@gmail.com>
2025-09-24 15:35:29 +0530

    add writeJSONResponse common method

--- handlers/movie_handlers.go
+++ handlers/movie_handlers.go
@@ -9,6 +9,14 @@ import (
 
 type MovieHandler struct{}
 
+func (h *MovieHandler) writeJSONResponse(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		// TODO: Log error
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
 func (h *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
 	movies := []models.Movie{
 		{
@@ -30,9 +38,29 @@ func (h *MovieHandler) GetTopMovies(w http.ResponseWriter, r *http.Request) {
 			Casting:     []models.Actor{{ID: 2, FirstName: "Christian"}},
 		},
 	}
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(movies); err != nil {
-		// TODO: Log error
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	h.writeJSONResponse(w, movies)
+}
+
+func (h *MovieHandler) GetRandomMovies(w http.ResponseWriter, r *http.Request) {
+	movies := []models.Movie{
+		{
+			ID:          1,
+			TMBD_ID:     181,
+			Title:       "The Hacker Random",
+			ReleaseYear: 2022,
+			Genres:      []models.Genre{{ID: 1, Name: "Thriller"}},
+			Keywords:    []string{},
+			Casting:     []models.Actor{{ID: 1, FirstName: "Max"}},
+		},
+		{
+			ID:          2,
+			TMBD_ID:     181,
+			Title:       "The Dark Knight Random",
+			ReleaseYear: 2008,
+			Genres:      []models.Genre{{ID: 2, Name: "Drama"}},
+			Keywords:    []string{},
+			Casting:     []models.Actor{{ID: 2, FirstName: "Christian"}},
+		},
 	}
+	h.writeJSONResponse(w, movies)
 }
